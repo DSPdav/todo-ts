61a0f48702d8e8bc6e9c139b31e89e2789a71217
DSPdav <davidsaronoputro@gmail.com>
2021-03-11 02:47:39 +0700

    test: menambahkan test case untuk form

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { getByTestId, render, screen } from '@testing-library/react';
 import App from './App';
+import Form from './ui/Form'
+import userEvent from '@testing-library/user-event';
 
 test('<App /> render <Main/> and <Footer/>', () => {
   render(<App />);
@@ -9,3 +11,13 @@ test('<App /> render <Main/> and <Footer/>', () => {
   const footerElement = screen.getByTestId('footer');
   expect(footerElement).toBeInTheDocument();
 });
+
+test('functionality of Form at <Main/>', async () => {
+  const setState = jest.fn();
+  render(<Form list={[{}]} setList={setState}/>);
+  await userEvent.type(screen.getByRole('textbox'), 'Doing testing');
+  expect(screen.getByRole('textbox')).toHaveValue('Doing testing');
+  await userEvent.click(screen.getByRole('button'))
+  expect(screen.getByRole('textbox')).toHaveValue('');
+  expect(setState).toHaveBeenCalledTimes(1);
+})
